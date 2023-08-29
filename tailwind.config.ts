6834c2534404bcfbca57bce4f7c63e9a7da0bf77
AdrianKnapp <adrknapp@gmail.com>
2023-08-29 20:46:43 -0300

    feat: first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: '#FFD836',
+      }
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-gradient-mask-image")],
 }
 export default config
