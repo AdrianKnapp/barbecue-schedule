963bbad2b445a3c357da83400bcbbf72e361b8a3
AdrianKnapp <adrknapp@gmail.com>
2023-08-30 20:04:25 -0300

    feat(component): created initial structure for button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import type { Metadata } from 'next';
 import { Raleway } from 'next/font/google';
 import Image from 'next/image';
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={raleway.className}>
-        <div className="relative flex flex-col w-[636px] min-h-[800px] mx-auto bg-primary shadow-lg mt-10">
+        <div className="relative flex flex-col w-[636px] min-h-[800px] mx-auto bg-primary-default shadow-lg mt-10">
           <div className="absolute top-0 left-0 gradient-mask-b-60 pointer-events-none">
             <Image
               src="/barbecue-pattern.svg"
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               height={200}
             />
           </div>
-          <h1 className="text-3xl font-bold text-center mt-20">Agenda de Churras</h1>
+          <h1 className="text-3xl text-center mt-20 font-extrabold">Agenda de Churras</h1>
           {children}
         </div>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Button from '@/components/ui/Button';
+
 export default function Home() {
   return (
     <div>
       <p>Home</p>
+      <Button>Button test</Button>
     </div>
   );
 }
