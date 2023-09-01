0cb30f831bf178505049d44f1da7fca99e9bc0e9
AdrianKnapp <adrknapp@gmail.com>
2023-09-01 17:47:04 -0300

    feat(component): adjusted add guest panel

--- src/components/common/BarbecueGuestsList/AddGuest/index.tsx
+++ src/components/common/BarbecueGuestsList/AddGuest/index.tsx
@@ -19,48 +19,56 @@ const AddGuest = () => {
   return isInEditMode ? (
     <div className="editor-container">
       <InputText id="editor" label="Nome" placeholder="Fulano" />
-      <Listbox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <Listbox.Button className="py-3.5 px-5 border border-dark-default w-full text-left">
-            <span className="block truncate">{selected.name}</span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-              </svg>
-            </span>
-          </Listbox.Button>
-          <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-            <Listbox.Options className="absolute mt-2 max-h-60 w-full overflow-auto bg-white border border-dark-default shadow-xl">
-              {people.map((person, personIdx) => (
-                <Listbox.Option
-                  key={personIdx}
-                  className={({ active }) =>
-                    `relative cursor-pointer select-none py-2 px-5 duration-default ${
-                      active ? 'bg-gray-100' : 'text-gray-900'
-                    }`
-                  }
-                  value={person}
+      <div className="select-contribution">
+        <p className="select-contribution-title">Contribuição</p>
+        <Listbox value={selected} onChange={setSelected}>
+          <div className="relative mt-1">
+            <Listbox.Button className="py-3.5 px-5 border border-dark-default w-full text-left focus:ring-2 focus:ring-dark-default outline-none">
+              <span className="block truncate">{selected.name}</span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
                 >
-                  {({ selected }) => (
-                    <>
-                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                        {person.name}
-                      </span>
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute mt-2 max-h-60 w-full overflow-auto bg-white border border-dark-default shadow-xl">
+                {people.map((person, personIdx) => (
+                  <Listbox.Option
+                    key={personIdx}
+                    className={({ active }) =>
+                      `relative cursor-pointer select-none py-2 px-5 duration-default ${
+                        active ? 'bg-gray-100' : 'text-gray-900'
+                      }`
+                    }
+                    value={person}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                          {person.name}
+                        </span>
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
       <div className="editor-button-group">
         <Button>Adicionar</Button>
         <Button variant="outline" onClick={handleToggleEditMode}>
